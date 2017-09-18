076912677fa5ea709856efae54405c52bc1b0552
Seba Belmar <si.belmar@gmail.com>
2017-09-18 09:06:46 -0300

    SensorController: Changed properties to support classes. Pseudocoded.

--- app/sensor_controller.rb
+++ app/sensor_controller.rb
@@ -4,23 +4,48 @@ class SensorController
   include Observable
   attr_reader :status, :state
 
-  def initialize(observer)
-    @status = 'on'
-    @state = []
-    @observer = observer
+  def initialize(observer, building)
+    @on             = false
+    @activated      = false
+    @building       = building
+    @sensor         = sensors
+    @appliance      = building
 
-    add_observer(@observer)
+    add_observer(@appliances)
   end
 
-  # Receives messages from sensors
+  # Turn on:
+  #   => Create all sensors per floor
+  #   => Create all apps per floor as off
+  #   => Sets levels of energy in use somewhere
+  def turn_on
+    @sensor.on
+    @appliance.on
+  end
+
+  # Turn to activated:
+  #   => Set all main light on
+  #   => Set all sub lights to motion
+  #   => Set all AC to on
+  #   => Runs energy analysis
+
+
+  ############## State Analisis     ####
+
+  ############## Appliance Analisis ####
+
+  ############## MESSAGES ##############
+  # Receives messages from sensors.
+  # Executes changes on 'state'
   def update(sensor)
-    # This should trigger a state analysis and update the
-    # Appliance tree (representation of the apliances)
+    # 1- This should trigger a state analysis
+    # 2- Updates the Appliance tree (representation of the apliances)
+    # 3- Send new Appliance Tree to Appliance via messages
     puts "Updated from a sensor..."
     self.state = state << sensor.id
   end
 
-  # Emits messges to appliances
+  # Emits messages to appliances
   def state=(state)
     changed
     @state = state
