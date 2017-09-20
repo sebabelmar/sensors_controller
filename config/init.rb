e94baf4dfc745a7c99571357cbc06a2426b9f5e9
Seba Belmar <si.belmar@gmail.com>
2017-09-20 14:53:30 -0300

    ConfigInit: Created config constants.

---
+++ config/init.rb
@@ -0,0 +1,50 @@
+FLOOR_CONFIG =
+  [
+    {
+      number: 1,
+      main_corridors: [{
+          number: 1,
+          sub_corridors: [{number:1},{number:2}]
+        }]
+    },
+    {
+      number: 2,
+      main_corridors: [{
+          number: 1,
+          sub_corridors: [{number:1},{number:2}]
+        }]
+    },
+  ]
+
+APPLIANCE_CONFIG = [
+      {
+        type: 'ac',
+        location: 'main',
+        qty_per_location: 1,
+        energy_consuption: 10
+      },
+      {
+        type: 'ac',
+        location: 'sub',
+        qty_per_location: 1,
+        energy_consuption: 10
+      },
+      {
+        type: 'light',
+        location: 'main',
+        qty_per_location: 1,
+        energy_consuption: 5
+      },
+      {
+        type: 'light',
+        location: 'sub',
+        qty_per_location: 1,
+        energy_consuption: 5
+      },
+    ]
+
+RESTRICTION = {
+    level: 'floor',
+    main_times: 10 ,
+    sub_times: 15
+  }
