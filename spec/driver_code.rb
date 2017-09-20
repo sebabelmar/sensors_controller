fc22dbe139da44c82456928b1420f36f8e9c5716
Seba Belmar <si.belmar@gmail.com>
2017-09-20 15:23:32 -0300

    DriverCode refactored using constants.

--- spec/driver_code.rb
+++ spec/driver_code.rb
@@ -1,81 +1,28 @@
 require_relative 'spec_helper'
 
 # # Driver Code
-floors_config =
-  [
-    {
-      number: 1,
-      main_corridors: [{
-          number: 1,
-          sub_corridors: [{number:1},{number:2}]
-        }]
-    },
-    {
-      number: 2,
-      main_corridors: [{
-          number: 1,
-          sub_corridors: [{number:1},{number:2}]
-        }]
-    },
-  ]
-
-appliances_config = [
-      {
-        type: 'ac',
-        location: 'main',
-        qty_per_location: 1,
-        energy_consuption: 10
-      },
-      {
-        type: 'ac',
-        location: 'sub',
-        qty_per_location: 1,
-        energy_consuption: 10
-      },
-      {
-        type: 'light',
-        location: 'main',
-        qty_per_location: 1,
-        energy_consuption: 5
-      },
-      {
-        type: 'light',
-        location: 'sub',
-        qty_per_location: 1,
-        energy_consuption: 5
-      },
-    ]
-
-restriction = {
-    level: 'floor',
-    main_times: 10 ,
-    sub_times: 15
-  }
-
 building_args =
   {
     category: 'hotel',
-    floors_config: floors_config,
-    appliances_config: appliances_config,
-    sensors: [],
-    restriction: restriction
+    floors_config: FLOOR_CONFIG,
+    appliances_config: APPLIANCE_CONFIG,
+    restriction: RESTRICTION
   }
-
 
 building = Building.new(building_args)
 
 valid_args =
   {
-    building: building,
+    building: @building,
     sensor: Sensor,
     appliance: Appliance
   }
 
-sensor_controller = OperationsController.new(valid_args)
+operations_controller = OperationsController.new(valid_args)
 
 Appliance.turn_on(building)
 Appliance.print_state
 Appliance.energy_report
 
-Sensor.turn_on(building, sensor_controller)
-p Sensor.all
+Sensor.turn_on(building, operations_controller)
+Sensor.all
